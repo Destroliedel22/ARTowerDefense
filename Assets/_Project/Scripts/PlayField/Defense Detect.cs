2d03b5f6347fa4065d875fd5c175182217b774db
Destroliedel22 <Jordiwalet@gmail.com>
2024-11-15 12:37:13 +0100

    turret place on plain
    need to have the code so you can only place stuff when plain is locked

--- Assets/_Project/Scripts/PlayField/Defense Detect.cs
+++ Assets/_Project/Scripts/PlayField/Defense Detect.cs
@@ -1,5 +1,6 @@
 using Oculus.Interaction;
 using Oculus.Interaction.HandGrab;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class DefenseDetection : MonoBehaviour
@@ -7,34 +8,32 @@ public class DefenseDetection : MonoBehaviour
     GameObject grabObject;
     bool isPlaced;
 
-    private void Awake()
-    {
-        grabObject = gameObject.GetComponentInChildren<CustomITransformer>().gameObject;
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Defense"))
         {
+            grabObject = collision.gameObject.GetNamedChild("[BuildingBlock] HandGrab");
             if (collision.gameObject.GetComponentInChildren<CustomITransformer>().IsGrabbed == false)
             {
-                collision.transform.rotation = Quaternion.Euler(0, 0, 0);
                 collision.gameObject.GetComponent<Rigidbody>().useGravity = false;
                 collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-                grabObject.SetActive(false);
+                collision.gameObject.transform.SetParent(transform);
+                collision.transform.rotation = Quaternion.Euler(0, 0, 0);
+                collision.gameObject.GetNamedChild("[BuildingBlock] HandGrab").SetActive(false);
                 isPlaced = true;
             }
         }
     }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Defense"))
-        {
-            collision.gameObject.GetComponent<Rigidbody>().useGravity = true;
-            collision.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-            grabObject.SetActive(true);
-            isPlaced = false;
-        }
-    }
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Defense"))
+    //    {
+    //        collision.gameObject.GetComponent<Rigidbody>().useGravity = true;
+    //        collision.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+    //        collision.gameObject.transform.SetParent(transform);
+    //        grabObject.SetActive(true);
+    //        isPlaced = false;
+    //    }
+    //}
 }
