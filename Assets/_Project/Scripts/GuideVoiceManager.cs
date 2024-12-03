39c1d8c443693fa713a72ae8cfc166fe5015bbfa
Destroliedel22 <Jordiwalet@gmail.com>
2024-12-03 14:37:42 +0100

    Guide Voice added with manager
    it doesnt replay if you dont do the action after a while or does not give feedforward

---
+++ Assets/_Project/Scripts/GuideVoiceManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideVoiceManager : MonoBehaviour
+{
+    [SerializeField] public List<AudioClip> audioClips = new List<AudioClip>();
+
+    private AudioSource audioSource;
+    private int audioNum;
+
+    private void Awake()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        audioNum = -1;
+        StartCoroutine(StartTutorial());
+    }
+
+    public void NextStep()
+    {
+        if(audioNum < audioClips.Count)
+        {
+            audioNum++;
+        }
+        audioSource.clip = audioClips[audioNum];
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+        StartCoroutine(StopAudio());
+    }
+
+    IEnumerator StartTutorial()
+    {
+        yield return new WaitForSeconds(audioSource.clip.length);
+        NextStep();
+    }
+
+    IEnumerator StopAudio()
+    {
+        yield return new WaitForSeconds(audioSource.clip.length + 0.5f);
+        audioSource.Stop();
+    }
+}
