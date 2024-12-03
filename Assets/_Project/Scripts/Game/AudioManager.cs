b753ead46c965e32d077593bfd1d56b45d0e722a
Inola <chickpea803@gmail.com>
2024-12-03 21:40:12 +0100

    Download audio + audio manager

---
+++ Assets/_Project/Scripts/Game/AudioManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("-------> Audio source <-------")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource SFXSource;
+
+    [Header("-------> Audio clips <-------")]
+    public AudioClip background;
+    public AudioClip newRound;
+    public AudioClip newWave;
+    public AudioClip enemyDeath;
+    public AudioClip enemyTakeDamage;
+    public AudioClip enemyHitWithHands;
+    public AudioClip baseDamage;
+    public AudioClip collectLoot;
+    public AudioClip buyInShop;
+    public AudioClip powerUpActive;
+    public AudioClip powerUpEnd;
+    public AudioClip turretShoot;
+    public AudioClip turretPlaced;
+    public AudioClip gameWon;
+    public AudioClip gameLost;
+    public AudioClip lostExplosion;
+    public AudioClip buttonPress;
+    public AudioClip fingerInteraction;
+
+    #region Singleton
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            // Check if there is something in this instance
+            if (_instance == null)
+            {
+                Debug.Log("AudioManager is empty");
+            }
+            // Return the instance to have it be read
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        // If instance is not empty, destroy this, meaning there can be only 1 audio manager
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+    private void Start()
+    {
+        // Play the background music
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    // Call this function where we want to play a sound effect
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
