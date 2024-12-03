3b9637adabd3a715b4c420efb859072f4be28299
Inola <chickpea803@gmail.com>
2024-12-03 20:46:19 +0100

    Update damage indicator enemies

--- Assets/_Project/Scripts/Enemies/EnemyHealth.cs
+++ Assets/_Project/Scripts/Enemies/EnemyHealth.cs
@@ -1,7 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    // Damage
+    [SerializeField] private GameObject damageTextPrefab;
+
     // Health
     private float maxHealth = 5;
     public float currentHealth;
@@ -12,9 +16,20 @@ public class EnemyHealth : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    // Instantiate the amount of damage it takes
+    private void ShowDamage(string text)
+    {
+        if (damageTextPrefab)
+        {
+            GameObject damagePrefab = Instantiate(damageTextPrefab, transform.position, Quaternion.identity);
+            damagePrefab.GetComponentInChildren<TextMeshPro>().text = text;
+        }
+    }
+
     // Take damage
     public void TakeDamage(float amount)
     {
+        ShowDamage("-" + amount.ToString());
         currentHealth -= amount;
     }
 }
